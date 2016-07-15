22d935a3c38e988962c007bbce17f296692265fd
NuccioJohn <nuccio.john@gmail.com>
2016-07-15 10:21:52 -0400

    Test Test

--- app/src/main/java/com/mac/training/applicationclass/MainActivity.java
+++ app/src/main/java/com/mac/training/applicationclass/MainActivity.java
@@ -2,12 +2,41 @@ package com.mac.training.applicationclass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainAct";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onStart: 1");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: 2");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onStart: 3");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onStart: 4");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onStart: 5");
+    }
+
 }
---
+++ app/src/main/java/com/mac/training/applicationclass/ServiceClass.java
@@ -0,0 +1,7 @@
+package com.mac.training.applicationclass;
+
+/**
+ * Created by admin on 7/15/2016.
+ */
+public class ServiceClass {
+}
